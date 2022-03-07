59ba6db5e186a31b89bd11dc8c0de6d6ff2b72e9
Daniel <Daniel@Daniels-MacBook-Pro.local>
2022-03-06 20:04:00 -0800

    updated mux dependencies

--- service/serve/go.mod
+++ service/serve/go.mod
@@ -2,4 +2,7 @@ module github.com/ggarcia209/portfolio-k8s/service/serve
 
 go 1.13
 
-require github.com/ggarcia209/portfolio-k8s v0.0.0-20220307025145-e9a5d98f1170
+require (
+	github.com/ggarcia209/portfolio-k8s v0.0.0-20220307034355-c24be5a913e8
+	github.com/ggarcia209/portfolio-k8s/service/util/server v0.0.0-20220307034355-c24be5a913e8
+)
--- service/serve/main.go
+++ service/serve/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/ggarcia209/portfolio-k8s/service/util/server"
 )
@@ -10,10 +9,5 @@ import (
 func main() {
 	// create http server and handler functions
 	fmt.Println("initializing http server...")
-	srv := server.InitHTTPServer("localhost:8081")
-	fmt.Printf("server address: %v\nread timeout: %v\nwrite timeout: %v\n",
-		srv.Addr, srv.ReadTimeout, srv.WriteTimeout)
-	fmt.Printf("listening at: '%v'...\n", srv.Addr)
-	server.RegisterHandlers()
-	log.Fatal(srv.ListenAndServe())
+	server.InitMuxRouter(8081)
 }
